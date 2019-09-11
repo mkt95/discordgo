4069dac15b26772a5d174868a713e4f20bbd74c5
tuckerrrrrrrrrrrr <tucker1014@hotmail.com>
2019-09-10 18:09:24 -0700

    fix inconsistent tts casing

--- restapi.go
+++ restapi.go
@@ -1719,7 +1719,7 @@ func (s *Session) ChannelMessageSendComplex(channelID string, data *MessageSend)
 func (s *Session) ChannelMessageSendTTS(channelID string, content string) (*Message, error) {
 	return s.ChannelMessageSendComplex(channelID, &MessageSend{
 		Content: content,
-		Tts:     true,
+		TTS:     true,
 	})
 }
 
